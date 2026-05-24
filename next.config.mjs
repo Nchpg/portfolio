26fd6f4b29b965e2e8b4a417fd2af95b29a442e3
nathan.champagne <nathan.champagne@epita.fr>
2026-05-24 21:59:37 +0200

    fix: video webp

--- next.config.mjs
+++ next.config.mjs
@@ -12,7 +12,7 @@ const nextConfig = {
       },
       {
         source: '/projects/(.*)',
-        headers: [{ key: 'Cache-Control', value: 'public, max-age=86400' }],
+        headers: [{ key: 'Cache-Control', value: 'public, max-age=31536000, immutable' }],
       },
     ];
   },
